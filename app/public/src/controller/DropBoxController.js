b251a6b96870a5f294411b43b62fc597ac67c572
moises78moura <moisesicdemoura@gmail.com>
2019-06-19 21:22:52 -0300

    inclusão de arquivos de estudo, foi incluído o diretório node_modules, haja visto que continuo os estudos na empresa e o proxy bloqueia o download de algumas libs.

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -1,3 +1,6 @@
+//import { resolve } from "dns";
+//import { rejects } from "assert";
+
 class DropBoxController{
 
     constructor(){
@@ -17,11 +20,39 @@ class DropBoxController{
         });
 
         this.inputFileEl.addEventListener('change', event =>{
-            console.log('event.target.files', event.target.files);
-            this.snackModalEl.getElementsByClassName.display = 'block';
+            this.uploadTask(event.target.files);
+            this.snackModalEl.style.display = 'block';
         });
 
 
+    }
+
+    uploadTask(files){
+        let promises = [];
+        //operador spread ...
+        [...files].forEach(file=>{
+            promises.push(new Promise((resolve, reject) => {
+                let ajax = new XMLHttpRequest();
+                ajax.open('POST', '/upload');
+                ajax.onload = event =>{
+                    try{
+                        resolve(JSON.parse(ajax.responseText));
+                    }catch (error){
+                        reject(error);
+                    }
+                };
+                ajax.onerror = event =>{
+                    reject(event);
+                };
+
+                let formData = new FormData();
+                formData.append('input-file', file);
+                ajax.send(formData);
+
+            }));
+        });
+
+        return Promise.all(promises);
     }
 
 }
